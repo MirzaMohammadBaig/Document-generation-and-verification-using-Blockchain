24f3178d95b442a1fbbe3f1e33caf368b413a1db
Mirza <mirzabaig.n321@gmail.com>
2022-11-15 19:58:40 +0530

    CRUD done

--- contracts/Cert.sol
+++ contracts/Cert.sol
@@ -1,4 +1,19 @@
 pragma solidity =0.5.16;
 contract Cert{
     uint public count=0;
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task)public  tasks;
+
+    constructor() public{
+        createTask("Default Task");
+    }
+
+    function createTask(string memory _content) public{
+        count ++;
+        tasks[count] = Task(count, _content, false);
+    }
 }
